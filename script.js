d4721a5439cdd59b5e3ac48d19f9992480b56a97
ur884241 <jordi1@outlook.com.br>
2024-09-28 01:51:03 +0000

    New Modifications

--- script.js
+++ script.js
@@ -79,3 +79,109 @@ generateAsciiSigil();
 
 // Regenerate ASCII sigil every 10 seconds
 setInterval(generateAsciiSigil, 3330);
+
+
+const canvas = document.getElementById('titleCanvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = 200;
+
+const particles = [];
+const text = 'GEMATRIA-SUM QUOTE FINDER';
+const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+class Particle {
+    constructor(x, y, char) {
+        this.x = x;
+        this.y = y;
+        this.char = char;
+        this.size = 12;
+        this.baseX = x;
+        this.baseY = y;
+        this.density = (Math.random() * 30) + 1;
+    }
+
+    draw() {
+        ctx.fillStyle = 'white';
+        ctx.font = '12px Courier';
+        ctx.fillText(this.char, this.x, this.y);
+    }
+
+    update() {
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y;
+        let distance = Math.sqrt(dx * dx + dy * dy);
+        let forceDirectionX = dx / distance;
+        let forceDirectionY = dy / distance;
+        let maxDistance = mouse.radius;
+        let force = (maxDistance - distance) / maxDistance;
+        let directionX = forceDirectionX * force * this.density;
+        let directionY = forceDirectionY * force * this.density;
+
+        if (distance < mouse.radius) {
+            this.x -= directionX;
+            this.y -= directionY;
+        } else {
+            if (this.x !== this.baseX) {
+                let dx = this.x - this.baseX;
+                this.x -= dx/10;
+            }
+            if (this.y !== this.baseY) {
+                let dy = this.y - this.baseY;
+                this.y -= dy/10;
+            }
+        }
+    }
+}
+
+function init() {
+    particles.length = 0;
+    let size = 24;
+    let x = (canvas.width - ctx.measureText(text).width) / 2;
+    let y = 100;
+
+    ctx.font = size + 'px Courier';
+    ctx.fillStyle = 'white';
+    ctx.fillText(text, x, y);
+
+    const pixelData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+
+    for (let y = 0; y < canvas.height; y += 4) {
+        for (let x = 0; x < canvas.width; x += 4) {
+            if (pixelData[(y * canvas.width + x) * 4 + 3] > 128) {
+                let posX = x + Math.random() * 4;
+                let posY = y + Math.random() * 4;
+                particles.push(new Particle(posX, posY, chars[Math.floor(Math.random() * chars.length)]));
+            }
+        }
+    }
+}
+
+const mouse = {
+    x: null,
+    y: null,
+    radius: 100
+}
+
+window.addEventListener('mousemove', function(event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+});
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < particles.length; i++) {
+        particles[i].draw();
+        particles[i].update();
+    }
+    requestAnimationFrame(animate);
+}
+
+init();
+animate();
+
+// Periodically reinitialize to create fading effect
+setInterval(() => {
+    init();
+}, 5000);
