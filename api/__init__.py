84af7b2a0ccab86102a205728c085b1699c8602d
ur884241 <jordi1@outlook.com.br>
2025-05-07 00:02:38 -0300

    add8

--- api/__init__.py
+++ api/__init__.py
@@ -2,4 +2,6 @@
 from api.core import fetch_text, find_matching_quotes
 from api.search_strategies import STRATEGY_FUNCTIONS, ALL_STRATEGIES
 
-__all__ = ['fetch_text', 'find_matching_quotes', 'STRATEGY_FUNCTIONS', 'ALL_STRATEGIES'] 
+__all__ = ['fetch_text', 'find_matching_quotes', 'STRATEGY_FUNCTIONS', 'ALL_STRATEGIES']
+
+ 
--- api/index.py
+++ api/index.py
@@ -1,7 +1,4 @@
 from api.search import handler, vercel_handler
 
 def lambda_handler(event, context):
-    return handler(event, context)
-
-def vercel_handler(request):
-    return VercelHandler(request) 
+    return handler(event, context) 
