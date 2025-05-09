4fdce7f85f22ee228b0e3d2189fea08331721572
ur884241 <jordi1@outlook.com.br>
2025-05-08 23:18:30 -0300

    Colour scheme for about

--- about.js
+++ about.js
@@ -490,5 +490,6 @@ function highlightCode(code) {
         .replace(/([a-zA-Z_$][a-zA-Z0-9_$]*):/g, '<span class="property">$1</span>:')
         .replace(/([;.,(){}[\]])/g, '<span class="punctuation">$1</span>')
         .replace(/\/(.*?)\//g, '<span class="regex">/$1/</span>')
-        .replace(/\b(parseInt|charCodeAt|test|map|join|slice|push)\b/g, '<span class="builtin">$1</span>');
+        .replace(/\b(parseInt|charCodeAt|test|map|join|slice|push)\b/g, '<span class="builtin">$1</span>')
+        .replace(/\n/g, '<br>'); // Add line breaks
 }
